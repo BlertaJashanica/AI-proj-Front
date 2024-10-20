2c933d43e4c230336df49ce8d8c3cfb74b0a486a
Blerta Jashanica <s129635@ap.be>
2024-10-20 20:21:17 +0200

    foto toegevoegd

--- TEST/src/app/app.component.spec.ts
+++ TEST/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'TEST' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TEST');
+    expect(app.outputText).toEqual('TEST');
   });
 
   it('should render title', () => {
