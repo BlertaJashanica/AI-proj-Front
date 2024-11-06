fcbe95e03454616a3ba503dcca6e47f017c101e2
Blerta Jashanica <s129635@ap.be>
2024-11-06 11:11:14 +0100

    frontend code

--- TEST/src/app/app.component.ts
+++ TEST/src/app/app.component.ts
@@ -13,16 +13,21 @@ import {TextEditorComponent} from './text-editor/text-editor.component'
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
   logoUrl: string = 'https://seeklogo.com/images/S/stad-antwerpen-logo-F659E48C62-seeklogo.com.png';
-
   outputText: string = '';
+  showMessage: boolean = false;  // Voeg deze regel toe
 
   onTextGenerated(text: string) {
     this.outputText = text;
+    this.showMessage = true;  // Zet showMessage op true wanneer tekst gegenereerd is
+  }
+
+  onOutputTextChanged(updatedText: string) {
+    this.outputText = updatedText;
   }
 
   onTextFinalized() {
     this.outputText = '';
+    this.showMessage = false;  // Zet showMessage op false na finaliseren
   }
 }
